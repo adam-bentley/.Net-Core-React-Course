fa080d0e12191bb56a1adbefd568b2a64343af7b
sp-racecontrol <racecontrol@santapod.com>
2021-07-03 18:29:51 +0100

    Section 16

---
+++ Application/Photos/Delete.cs
@@ -0,0 +1,59 @@
+﻿using Application.Core;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Photos
+{
+    public class Delete
+    {
+        public class Command: IRequest<Result<Unit>>
+        {
+            public string Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly DataContext context;
+            private readonly IPhotoAccessor photoAccessor;
+            private readonly IUserAccessor userAccessor;
+
+            public Handler(DataContext context, IPhotoAccessor photoAccessor, IUserAccessor userAccessor)
+            {
+                this.context = context;
+                this.photoAccessor = photoAccessor;
+                this.userAccessor = userAccessor;
+            }
+
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var user = await context.Users.Include(p => p.Photos).FirstOrDefaultAsync(x => x.UserName == userAccessor.GetUsername(), cancellationToken: cancellationToken);
+
+                if (user == null) return null;
+
+                var photo = user.Photos.FirstOrDefault(x => x.Id == request.Id);
+
+                if (photo == null) return null;
+
+                if (photo.IsMain) return Result<Unit>.Failure("Cannot delete main photo");
+
+                var result = await photoAccessor.DeletePhoto(photo.Id);
+
+                if (result == null) return Result<Unit>.Failure("Problem deleting from cloud");
+
+                user.Photos.Remove(photo);
+
+                var success = await context.SaveChangesAsync(cancellationToken) > 0;
+
+                if(success) return Result<Unit>.Sucess(Unit.Value);
+
+                return Result<Unit>.Failure("Problem deleting from API");
+
+            }
+        }
+    }
+}
---
+++ Application/Profiles/Details.cs
@@ -0,0 +1,38 @@
+﻿using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Profiles
+{
+    public class Details
+    {
+        public class Query : IRequest<Result<Profile>>
+        {
+            public string Username { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<Profile>>
+        {
+            private readonly DataContext context;
+            private readonly IMapper mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                this.context = context;
+                this.mapper = mapper;
+            }
+
+            public async Task<Result<Profile>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var user = await context.Users.ProjectTo<Profile>(mapper.ConfigurationProvider).SingleOrDefaultAsync(x => x.Username == request.Username, cancellationToken: cancellationToken);
+
+                return Result<Profile>.Sucess(user);
+            }
+        }
+    }
+}
